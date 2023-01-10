a348ad11c26ebbdfce67e8d215ac57137815640d
EC2 Default User <sigurokaru@gmail.com>
2023-01-10 05:56:56 +0000

    11

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,15 @@
 class PostImage < ApplicationRecord
     
-    has_one_attached :iamge
+    has_one_attached :image
     belongs_to :user
+    
+    def get_image
+        if image.attached?
+            image
+        else
+            'no_image.jpg'
+        end
+    end
     
     def get_image
         unless image.attached?
